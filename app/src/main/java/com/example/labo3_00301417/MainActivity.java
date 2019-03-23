2495a9f7d6eeaafe1ce0d7e01cf5458588091600
d1egopolanco <00301417@uca.edu.sv>
2019-03-23 00:26:51 -0600

    salto de fe

--- app/src/main/java/com/example/labo3_00301417/MainActivity.java
+++ app/src/main/java/com/example/labo3_00301417/MainActivity.java
@@ -10,30 +10,39 @@ import com.example.labo3_00301417.utilis.AppConstant;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText mEdit;
-    private Button mButton,mButtonShare;
+    private EditText mUser, mPassword, mMail, mGender;
+    private Button mButton, mButtonShare;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mEdit=findViewById(R.id.et_text);
-        mButton = findViewById(R.id.btn_send);
+        mUser = findViewById(R.id.user);
+        mPassword = findViewById(R.id.password);
+        mMail = findViewById(R.id.mail);
+        mGender = findViewById(R.id.gender);
+
         mButtonShare = findViewById(R.id.btn_share);
-        mButton.setOnClickListener(v-> {
-            String text = mEdit.getText().toString();
-            Intent mIntent = new Intent(MainActivity.this,newactivity.class);
-            mIntent.putExtra(AppConstant.TEXT_kEY,text);
-            startActivity(mIntent);
+
+
+        mButton.setOnClickListener(v -> {
+
+            mButtonShare.setOnClickListener(v -> {
+                String user = mUser.getText().toString();
+                String password = mPassword.getText().toString();
+                String mail = mMail.getText().toString();
+                String gender = mGender.getText().toString();
+                Intent mIntent = new Intent(MainActivity.this, newactivity.class);
+                mIntent.putExtra("user", user);
+                mIntent.putExtra("password", password);
+                mIntent.putExtra("mail", mail);
+                mIntent.putExtra("gender", gender);
+
+                startActivity(mIntent);
+
+
             });
-        mButtonShare.setOnClickListener(v->{
-            String text =mEdit.getText().toString();
-            Intent mIntent = new Intent();
-            mIntent.setType("text/plain");
-            mIntent.setAction(Intent.ACTION_SEND);
-            mIntent.putExtra(Intent.EXTRA_TEXT,text);
-            startActivity(mIntent);
         });
     }
 }
